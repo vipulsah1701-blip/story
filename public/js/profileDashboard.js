cb14432783edd76c812411f71242db1b04b3def0
Vipul Sah <vipulsah1701@gmail.com>
2026-04-03 14:00:08 +0530

    Update profileDashboard.js

--- public/js/profileDashboard.js
+++ public/js/profileDashboard.js
@@ -3,28 +3,34 @@ const BASE_URL = "https://story-9mch.onrender.com";
 fetch(`${BASE_URL}/user/me`, {
   credentials: "include"
 })
-.then(res => res.json())
+.then(res => {
+  if(res.status === 401){
+    alert("Please login first");
+    window.location.href = "login.html";
+    return;
+  }
+  return res.json();
+})
 .then(data => {
 
-  const user = data.user;
-  const stories = data.stories;
+  if(!data) return;
 
   document.getElementById("userInfo").innerHTML = `
     <div class="card">
-      <h3>${user.username}</h3>
-      <p>Email: ${user.email}</p>
+      <h3>${data.user.username}</h3>
+      <p>Email: ${data.user.email}</p>
     </div>
   `;
 
   const div = document.getElementById("myStories");
   div.innerHTML = "";
 
-  if (!stories || stories.length === 0) {
+  if (!data.stories || data.stories.length === 0) {
     div.innerHTML = "<p>No stories posted yet.</p>";
     return;
   }
 
-  stories.forEach(s => {
+  data.stories.forEach(s => {
     div.innerHTML += `
       <div class="card">
         <h3>${s.title}</h3>
@@ -36,7 +42,6 @@ fetch(`${BASE_URL}/user/me`, {
 
 });
 
-// DELETE
 function deleteStory(id) {
   fetch(`${BASE_URL}/stories/${id}`, {
     method: "DELETE",
